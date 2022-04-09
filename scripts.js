8b7ed8b6598488e1c6a371c95c3c17e48a1ca136
Yugo <yugosakamoto0717@gmail.com>
2022-04-09 14:30:28 -0300

    disposição das cartas pronta

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
-const board = document.querySelector("ul")
-const cartas = document.querySelectorAll("li")
+const board = document.querySelector(".jogo")
+const cartas = []
+const imagensFrente = [
+    `<img src="imgs/bobrossparrot.gif" class="frente"/>`,
+    `<img src="imgs/explodyparrot.gif" class="frente"/>`,
+    `<img src="imgs/fiestaparrot.gif" class="frente"/>`,
+    `<img src="imgs/metalparrot.gif" class="frente"/>`,
+    `<img src="imgs/revertitparrot.gif" class="frente"/>`,
+    `<img src="imgs/tripletsparrot.gif" class="frente"/>`,
+    `<img src="imgs/unicornparrot.gif" class="frente"/>`
+]
+const imagemVerso = `<img src="imgs/back.png" class="verso"/>`
 let i = 0
 let j = 0
+
 let numCartas = Number(prompt("Com quantas cartas deseja jogar?"));
 while (numCartas%2!=0 || numCartas < 4 || numCartas > 14) {
     numCartas = Number(prompt("Com quantas cartas deseja jogar?"));
 }
     while (i<(numCartas/2)){
-        cartas.push(`<li class="carta${i}"></li>`)
-        cartas.push(`<li class="carta${i}"></li>`)
+        cartas.push(`<div class="carta">${imagemVerso + imagensFrente[i]}</div>`)
+        cartas.push(`<div class="carta">${imagemVerso + imagensFrente[i]}</div>`)
         i++
     }
     cartas.sort(comparador)
